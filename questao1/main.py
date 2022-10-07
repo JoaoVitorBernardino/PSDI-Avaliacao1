2a9b45d842ef447eb3f9f92f69d7330dbd5912a7
JoaoVitorBernardino <jvitorbernardino@gmail.com>
2022-10-07 19:58:18 -0300

    Add questao 1 e 2

---
+++ questao1/main.py
@@ -0,0 +1,28 @@
+import cv2 as cv
+
+image = cv.imread("questao1/pokemon.jpg")
+hsvImg = cv.cvtColor(image, cv.COLOR_BGR2HSV)
+
+maskVermelho = cv.inRange(hsvImg, (0, 50, 50), (17, 255, 255))
+cntVermelho = cv.findContours(maskVermelho, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)[0]
+cntVermelho = sorted(cntVermelho, key=cv.contourArea, reverse=True)[:2]
+
+if cntVermelho:
+    for cnt in cntVermelho:
+        (cx, cy), r = cv.minEnclosingCircle(cnt)
+        cv.putText(image, "Red", (int(cx), int(cy)), cv.FONT_HERSHEY_COMPLEX, 0.9, (0, 0, 0), 2)
+        cv.circle(image, (int(cx), int(cy)), int(r), (0, 0, 0), 3)
+
+maskVerde = cv.inRange(hsvImg, (30, 50, 50), (80, 255, 255))
+cntVerde = cv.findContours(maskVerde, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)[0]
+cntVerde = sorted(cntVerde, key=cv.contourArea, reverse=True)[:2]
+
+if cntVerde:
+    for cnt in cntVerde:
+        (cx, cy), r = cv.minEnclosingCircle(cnt)
+        cv.putText(image, "Green", (int(cx), int(cy)), cv.FONT_HERSHEY_COMPLEX, 0.9, (0, 0, 0), 2)
+        cv.circle(image, (int(cx), int(cy)), int(r), (0, 0, 0), 3)
+
+
+cv.imshow("image", image)
+cv.waitKey(0)
---
+++ questao2/main2.py
@@ -0,0 +1,34 @@
+import cv2 as cv
+
+video = cv.VideoCapture("pixar.mp4")
+
+tracker = cv.legacy.TrackerCSRT_create()
+success, image = video.read()
+box = cv.selectROI("Tracking", image, False)
+tracker.init(image, box)
+
+
+def retangulo(image, box):
+    x, y, w, h = int(box[0]), int(box[1]), int(box[2]), int(box[3])
+    cv.rectangle(image, (x, y), ((x + w), (y + h)), (255, 0, 0), 3, 1)
+    cv.putText(image, "Frames", (75, 80), cv.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
+
+
+while True:
+    timer = cv.getTickCount()
+    success, image = video.read()
+
+    success, box = tracker.update(image)
+
+    if success:
+        retangulo(image, box)
+    else:
+        cv.putText(image, "Lost", (75, 80), cv.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0), 2)
+
+    fps = cv.getTickFrequency() / (cv.getTickCount() - timer)
+    cv.putText(image, str(int(fps)) + " FPS", (75, 50), cv.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0), 2)
+    cv.putText(image, "Sair (s)", (250, 350), cv.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 2)
+    cv.imshow("Tracking Object", image)
+
+    if cv.waitKey(1) & 0xff == ord('s'):
+        break
